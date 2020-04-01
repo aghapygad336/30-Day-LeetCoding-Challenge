dc843a7136588a519254e62131dd8f11edfab236
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2020-04-01 17:34:27 +0200

    Create singleNumber.java

---
+++ singleNumber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        {
+        Map<Integer, Integer> count = new HashMap();
+        for (int x: nums) {
+            count.put(x, count.getOrDefault(x, 0) + 1);
+        }
+
+        for (int k: count.keySet())
+            if (count.get(k) ==1)
+                return k;
+
+        throw null;
+    }
+    }
+}
